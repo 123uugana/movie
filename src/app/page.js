56ff130741928fea7c3670e180ca901e27cf802e
123uugana <uuganbayrxx0716@gmail.com>
2026-05-26 14:59:20 +0800

    Add animated 3D movie selector

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import HeroCarousel from "@/components/HeroCarousel";
+import CinemaOrbit3D from "@/components/CinemaOrbit3D";
 import MovieSection from "@/components/MovieSection";
 import { getMovies } from "@/lib/movie-service";
 
@@ -10,6 +11,7 @@ export default async function Home() {
   return (
     <main className="page">
       <HeroCarousel movies={movies.slice(0, 3)} />
+      <CinemaOrbit3D movies={movies.slice(0, 10)} />
 
       <MovieSection title="Popular Movies" movies={movies.slice(0, 8)} />
       <MovieSection title="Top Rated" movies={movies.slice(3, 9)} />
